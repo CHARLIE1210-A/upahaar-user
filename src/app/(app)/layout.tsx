72040e9becf1890f57db21a33d32cd11c61d0615
Ashutosh Singh <ash04singh@gmail.com>
2025-08-06 21:08:20 +0000

    still have the issue

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,14 +1,9 @@
 import AppShell from '@/components/layout/AppShell';
-import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function AppPagesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <SidebarProvider>
-      <AppShell>{children}</AppShell>
-    </SidebarProvider>
-  );
+  return <AppShell>{children}</AppShell>;
 }
